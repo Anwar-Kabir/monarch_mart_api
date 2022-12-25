10b96273122986a6d0dd5025bd64b2f3d6b984bc
RootHex200 <sabiturrahman60@gmail.com>
2022-12-25 15:00:26 +0600

    add-error-handler

--- src/server.js
+++ src/server.js
@@ -35,6 +35,15 @@ mongoose.connect(mongodbPath).then(function(){
 
 });
 
+app.use((req,res,next)=>{
+    res.status(404).json({message:"No route Found"})
+})
+
+//server error handle
+app.use((erro,req,res,next)=>{
+    res.status(500).json({message:"500 Internal Server Error"})
+})
+
 const PORT = process.env.PORT || 5000;;
 app.listen(PORT, function(){
     console.log("Server start at port:" + PORT);
